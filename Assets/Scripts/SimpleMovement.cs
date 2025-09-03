0d5d80813d4ab03751f18926836556a2d7737c15
Stonger <49844364+Stonger44@users.noreply.github.com>
2025-09-02 17:42:52 -0700

    Challenge Complete!

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class SimpleMovement : MonoBehaviour
 {
-    
+    [Header("Movement")]
+    [SerializeField] private float _movementSpeed = 3f;
+    private Vector2 _moveDirection;
+
+    [Space(10)]
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _moveDirection = Vector2.left;
+            _spriteRenderer.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _moveDirection = Vector2.right;
+            _spriteRenderer.flipX = false;
+        }
+        else
+        {
+            _moveDirection = Vector2.zero;
+        }
+
+        this.transform.Translate(_moveDirection * (_movementSpeed * Time.deltaTime));
+    }
 }
