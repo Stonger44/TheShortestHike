833962ddc821d9b9da493c164cab1bf9ad0be28f
Stonger <49844364+Stonger44@users.noreply.github.com>
2025-09-02 18:12:26 -0700

    Experiment with Horizontal axis input

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -5,6 +5,7 @@ public class SimpleMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float _movementSpeed = 3f;
     private Vector2 _moveDirection;
+    private float _horizontalInput;
 
     [Space(10)]
     [SerializeField] private SpriteRenderer _spriteRenderer;
@@ -15,6 +16,11 @@ public class SimpleMovement : MonoBehaviour
     }
 
     private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
@@ -32,5 +38,22 @@ public class SimpleMovement : MonoBehaviour
         }
 
         this.transform.Translate(_moveDirection * (_movementSpeed * Time.deltaTime));
+    }
+
+    private void Move2()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        // print(horizontalInput);
+
+        if (_horizontalInput < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (_horizontalInput > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        this.transform.Translate(_horizontalInput * (_movementSpeed * Time.deltaTime), 0, 0);
     }
 }
